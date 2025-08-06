4659c0876153459da5e658b0df5d61157904237d
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-08-06 09:47:48 +0530

    Time: 1 ms (72.32%), Space: 45.7 MB (66.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,35 +2,21 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
+        int[] markRow=new int[n];
+        int[] markCol=new int[m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    markRow(matrix,i,m);
-                    markCol(matrix,j,n);
+                    markRow[i]=1;
+                    markCol[j]=1;
                 }
             }
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(matrix[i][j]==-214748)
+                if(markRow[i]==1 || markCol[j]==1)
                     matrix[i][j] = 0;
                 }
-            }
-        }
-        private void markRow(int[][] matrix,int i,int m)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]!=0)
-                    matrix[i][j]=-214748;
-            }
-        }
-        private void markCol(int[][] matrix,int j,int n)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(matrix[i][j]!=0)
-                    matrix[i][j]=-214748;
             }
         }
 }
