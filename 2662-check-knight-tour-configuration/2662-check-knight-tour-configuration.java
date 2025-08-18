3f320d84eb226aa9394ecba7f2c8cafcc1cb3aa2
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-08-18 10:32:13 +0530

    Time: 2 ms (9.67%), Space: 44.6 MB (37.7%) - LeetHub

--- 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean checkValidGrid(int[][] grid) {
-        // return isValid(grid, grid.length, 0, 0, 0);
         int n = grid.length;
         int[][] pos = new int[n * n][2];
 
@@ -9,7 +8,7 @@ class Solution {
                 pos[grid[i][j]] = new int[] { i, j };
             }
         }
-        
+
         if (pos[0][0] != 0 || pos[0][1] != 0)
             return false;
 
@@ -24,23 +23,4 @@ class Solution {
         }
         return true;
     }
-
-    // private boolean isValid(int[][] grid, int n, int row, int col, int expVal) {
-    //     if (row < 0 || col < 0 || row >= n || col >= n || grid[row][col] != expVal)
-    //         return false;
-
-    //     if (grid[row][col] == n * n - 1)
-    //         return true;
-
-    //     boolean ans1 = isValid(grid, n, row - 2, col + 1, expVal + 1);
-    //     boolean ans2 = isValid(grid, n, row - 1, col + 2, expVal + 1);
-    //     boolean ans3 = isValid(grid, n, row + 1, col + 2, expVal + 1);
-    //     boolean ans4 = isValid(grid, n, row + 2, col + 1, expVal + 1);
-    //     boolean ans5 = isValid(grid, n, row + 2, col - 1, expVal + 1);
-    //     boolean ans6 = isValid(grid, n, row + 1, col - 2, expVal + 1);
-    //     boolean ans7 = isValid(grid, n, row - 1, col - 2, expVal + 1);
-    //     boolean ans8 = isValid(grid, n, row - 2, col - 1, expVal + 1);
-
-    //     return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8;
-    // }
 }
