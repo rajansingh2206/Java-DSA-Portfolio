5ceef82d318087416ce7ead1cdba76863610b16f
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-08-18 10:31:44 +0530

    Time: 2 ms (9.67%), Space: 44.4 MB (67.05%) - LeetHub

--- 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean checkValidGrid(int[][] grid) {
         // return isValid(grid, grid.length, 0, 0, 0);
-        int n=grid.length;
-        int pos[][]=new int[n*n][2];
+        int n = grid.length;
+        int[][] pos = new int[n * n][2];
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                pos[grid[i][j]]=new int[]{ i , j};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pos[grid[i][j]] = new int[] { i, j };
             }
         }
-        if(pos[0][0]!=0 || pos[0][1]!=0)
-            return false;
         
-        for(int i=1;i<n*n;i++)
-        {
-            int r1=pos[i-1][0], c1=pos[i-1][1];
-            int r2=pos[i][0], c2=pos[i][1];
+        if (pos[0][0] != 0 || pos[0][1] != 0)
+            return false;
 
-            int rowDiff=Math.abs(r1-r2), colDiff=Math.abs(c1-c2);
+        for (int i = 1; i < n * n; i++) {
+            int r1 = pos[i - 1][0], c1 = pos[i - 1][1];
+            int r2 = pos[i][0], c2 = pos[i][1];
 
-            if( !((rowDiff==2 && colDiff==1) || (rowDiff==1 && colDiff==2)) )
+            int rowDiff = Math.abs(r1 - r2), colDiff = Math.abs(c1 - c2);
+
+            if (!((rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2)))
                 return false;
         }
         return true;
