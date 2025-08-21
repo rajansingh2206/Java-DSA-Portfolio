995af0ff0ddd97a216eacbea2750aa3bf656cbe8
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-08-21 10:45:00 +0530

    Time: 10 ms (80.49%), Space: 44.7 MB (91.92%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,17 +2,26 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
         List<Integer> ans=new ArrayList<>();
-        int anagramSize=p.length();
-        char pchar[]=p.toCharArray();
-        Arrays.sort(pchar);
-        String sortedP=new String(pchar);
-        for(int i=0;i<s.length()-anagramSize+1;i++)
+        int []p_char_count=new int[26];
+        int []s_char_count=new int[26];
+
+        int p_length=p.length();
+        int s_length=s.length();
+
+        if(p_length>s_length) return ans;
+
+        for(int i=0;i<p_length;i++)
         {
-            String subString=s.substring(i,i+anagramSize);
-            char []element=subString.toCharArray();
-            Arrays.sort(element);
-            if(sortedP.equals(new String(element)))
-                ans.add(i);
+            p_char_count[p.charAt(i)-'a']++;
+            s_char_count[s.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(p_char_count,s_char_count))  ans.add(0);
+
+        for(int i=p_length;i<s_length;i++)
+        {
+            s_char_count[s.charAt(i)-'a']++;
+            s_char_count[s.charAt(i-p_length)-'a']--;
+            if(Arrays.equals(p_char_count,s_char_count))  ans.add(i-p_length+1);
         }
         return ans;
     }
