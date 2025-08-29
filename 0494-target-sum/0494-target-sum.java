c30f14681ea209e6932e2b37f8f76025ccec483a
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-08-29 17:57:22 +0530

    Time: 7 ms (67.58%), Space: 44.9 MB (29.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,27 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        
-        return recursion(nums,target,0,0);
+        int totalSum=0;
+        for(int val:nums)
+            totalSum+=val;
+        Integer dp[][]=new Integer[nums.length+1][2*totalSum+1];
+        return recursion(nums,target,0,0,dp,totalSum);
     }
-    public int recursion(int []nums,int target,int idx,int currSum)
+    public int recursion(int []nums,int target,int idx,int currSum,Integer dp[][],int totalSum)
     {
         if(idx==nums.length)
             return currSum==target?1:0;
 
+        int offsetSum=currSum+totalSum;
+        if(dp[idx][offsetSum]!=null)
+            return dp[idx][offsetSum];
+
         //take +ve
-        int ans1=recursion(nums,target,idx+1,currSum+nums[idx]);
+        int ans1=recursion(nums,target,idx+1,currSum+nums[idx],dp,totalSum);
 
         //take -ve
-        int ans2=recursion(nums,target,idx+1,currSum-nums[idx]);
+        int ans2=recursion(nums,target,idx+1,currSum-nums[idx],dp,totalSum);
 
-        return ans1+ans2;
+        dp[idx][offsetSum]=ans1+ans2;
+        return dp[idx][offsetSum];
     }
 }
