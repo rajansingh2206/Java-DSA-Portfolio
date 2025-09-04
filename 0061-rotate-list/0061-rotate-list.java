4b2f29406db4381efe098b36e7fff5d87b30ca83
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-09-05 00:16:32 +0530

    Time: 0 ms (100%), Space: 42.8 MB (38.01%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,30 +10,49 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode newHead = rotateList(head, k);
+        // ListNode newHead = rotateList(head, k);
+        if (head == null || head.next == null || k == 0)
+            return head;
+        int n = 1;
+        ListNode oldTail = head;
+        while (oldTail.next != null) {
+            oldTail = oldTail.next;
+            n++;
+        }
+        oldTail.next = head;
+
+        ListNode newTail = head;
+        int steps = n - k % n - 1;
+        for (int i = 0; i < steps; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
         return newHead;
     }
 
-    private ListNode rotateList(ListNode head, int k) {
-        if(head==null || head.next==null || k==0)   return head;
-        int len=1;
-        ListNode temp=head;
-        while(temp.next!=null)
-        {
-            temp=temp.next;
-            len++;
-        }
-        for (int i = 0; i < k%len; i++) {
-            ListNode prev = head;
-            ListNode curr = head.next;
-            while (curr.next != null) {
-                curr = curr.next;
-                prev = prev.next;
-            }
-            curr.next = head;
-            head = curr;
-            prev.next = null;
-        }
-        return head;
-    }
+    // private ListNode rotateList(ListNode head, int k) {
+    //     if(head==null || head.next==null || k==0)   return head;
+    //     int len=1;
+    //     ListNode temp=head;
+    //     while(temp.next!=null)
+    //     {
+    //         temp=temp.next;
+    //         len++;
+    //     }
+    //     for (int i = 0; i < k%len; i++) {
+    //         ListNode prev = head;
+    //         ListNode curr = head.next;
+    //         while (curr.next != null) {
+    //             curr = curr.next;
+    //             prev = prev.next;
+    //         }
+    //         curr.next = head;
+    //         head = curr;
+    //         prev.next = null;
+    //     }
+    //     return head;
+    // }
 }
