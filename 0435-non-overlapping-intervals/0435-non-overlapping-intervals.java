10cf9a344b4e5f8ac40c713bc538a471d463eac9
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-09-22 10:21:27 +0530

    Time: 65 ms (6.97%), Space: 73.8 MB (27%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,27 +1,15 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
 
-        int intervals_with_index[][] = new int[intervals.length][3];
-        for (int i = 0; i < intervals.length; i++) {
-            intervals_with_index[i][0] = i;
-            intervals_with_index[i][1] = intervals[i][0];
-            intervals_with_index[i][2] = intervals[i][1];
-        }
-        Arrays.sort(intervals_with_index, Comparator.comparingDouble(o -> o[2]));
-        // for (int i = 0; i < intervals.length; i++) {
-        //     System.out.print(intervals_with_index[i][0] + " " +
-        //             intervals_with_index[i][1] + " " +
-        //             intervals_with_index[i][2]);
-        //     System.out.println();
-        // }
+        Arrays.sort(intervals,Comparator.comparingDouble(o->o[1]));
         int count = 0;
-        int last_end_time = intervals_with_index[0][2];
+        int last_end_time = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals_with_index[i][1] < last_end_time) {
+            if (intervals[i][0] < last_end_time) {
                 count++;
             }
             else
-                last_end_time=intervals_with_index[i][2];
+                last_end_time=intervals[i][1];
         }
         return count;
     }
