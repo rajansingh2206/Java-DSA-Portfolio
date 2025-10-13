ff7a158c35e77de2191b8f2905be8fbe92e65a8a
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-10-13 19:27:36 +0530

    Time: 18 ms (62.19%), Space: 43.8 MB (90.32%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,26 +1,35 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
+        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n; j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) continue;
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
-                    if (sum == (long) target) {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            for(int j=i+1;j<n;j++)
+            {
+                if(j>i+1 && nums[j-1]==nums[j]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l)
+                {
+                    long sum=(long)nums[i]+(long)nums[j]+(long)nums[k]+(long)nums[l];
+                    // System.out.print(nums[i]+" + "+nums[j]+" + "+nums[k]+" + "+nums[l]);
+                    if(sum==(long)target)
+                    {
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
-                    } else if (sum < (long) target)
+                        while(k<l && nums[k-1]==nums[k])
+                            k++;
+                        while(k<l && nums[l+1]==nums[l])
+                            l--;
+                    }
+                    else if(sum<(long)target)
                         k++;
                     else
                         l--;
+                    // System.out.println();
                 }
             }
         }
