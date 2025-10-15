8a70476276d654a72e8b42f71a38e6fe4d0d3fb7
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-10-15 10:18:10 +0530

    Time: 3 ms (90.99%), Space: 43.8 MB (88.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,51 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int maxLen = 0;
-        int n = s.length();
-
-        // Map<Character,Integer> map = new LinkedHashMap<>();
-        // int lp = 0, rp = 0;
-        // while (rp < n) {
-        //     char curr = s.charAt(rp);
-        //     if (map.containsKey(curr) && map.get(curr)>=lp)
-        //         lp = map.get(curr) + 1;
-        //     map.put(curr,rp);
-        //     int currLen = rp - lp + 1;
-        //     maxLen = Math.max(maxLen, currLen);
-        //     rp++;
-        // }
-        int hash[]=new int[256];
-        Arrays.fill(hash,-1);
-        int lp=0,rp=0;
-        while(rp<n)
+        int idx[]=new int[256];
+        Arrays.fill(idx,-1);
+        int left=0;
+        int right=0;
+        int maxLen=0;
+        int n=s.length();
+        while(right<n)
         {
-            int curr=s.charAt(rp);
-            if(hash[curr]!=-1 && hash[curr]>=lp)
-                lp=hash[curr]+1;
-            hash[curr]=rp;
-            int currLen=rp-lp+1;
+            char currentChar=s.charAt(right);
+            if(idx[currentChar]>=left && idx[currentChar]!=-1)
+            {
+                left=idx[currentChar]+1;
+            }
+            idx[currentChar]=right;
+            int currLen=right-left+1;
             maxLen=Math.max(maxLen,currLen);
-            rp++;
+            right++;
         }
-        // Set<Character> set=new LinkedHashSet<>();
-        // for(int i=0;i<n;i++)
-        // {
-        //     int currLen=1;
-        //     set=new LinkedHashSet<>();
-        //     set.add(s.charAt(i));
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         maxLen=Math.max(currLen,maxLen);
-        //         if(set.add(s.charAt(j))==false)
-        //         {
-        //             break;
-        //         }
-        //         else
-        //         {
-        //             currLen++;
-        //         }
-        //     }
-        // }
         return maxLen;
     }
 }
