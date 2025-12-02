1efb0aec506b3946e58323b51cbfd2411f369206
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2025-12-02 10:24:38 +0530

    Time: 17 ms (29.99%), Space: 47.5 MB (12.22%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -6,21 +6,16 @@ class Solution {
             map.put(c,map.getOrDefault(c,0)+1);
         }
         List<Map.Entry<Character,Integer>> list=new ArrayList<>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Character,Integer>>(){
-            public int compare(Map.Entry<Character,Integer> e1,Map.Entry<Character,Integer> e2){
-                return e2.getValue()-e1.getValue();
-            }
-        }
-        );
-        String ans="";
+        list.sort((e1,e2)-> e2.getValue().compareTo(e1.getValue()));
+        StringBuffer sb=new StringBuffer();
         for(Map.Entry<Character,Integer> m:list){
             int count=m.getValue();
             int cnt=0;
             while(cnt<count){
-                ans+=m.getKey();
+                sb.append(m.getKey());
                 cnt++;
             }
         }
-        return ans;
+        return sb.toString();
     }
 }
