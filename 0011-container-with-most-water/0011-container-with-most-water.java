1f182eb7468ceb74568d248171ec30a3b6897b4d
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2026-01-06 10:12:04 +0530

    Time: 3 ms (97.08%), Space: 77.3 MB (48.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,33 +1,21 @@
 class Solution {
     public int maxArea(int[] height) {
-      int maxArea=Integer.MIN_VALUE;
-      int wdth=0;
-      int ht=0;
-      int n=height.length;
-      int lp=0,rp=n-1;
-      while(lp<rp)
-      {
-        int currArea=0;
-        wdth=rp-lp;
-        ht=Math.min(height[lp],height[rp]);
-        currArea=ht*wdth;
-        maxArea=Math.max(currArea,maxArea);
-        if(height[lp]<height[rp]) lp++; else rp--;
-      }
-
-    //Brute Force - O(n^2)
-    //   for(int i=0;i<n;i++)
-    //   {
-    //     int currArea=0;
-    //      for(int j=i+1;j<n;j++)
-    //      {
-    //         wdth=j-i;
-    //         ht=Math.min(height[i],height[j]);
-    //         currArea=wdth*ht;
-    //         // System.out.println("Height: "+ht+" Width: "+wdth+" currArea:"+currArea);
-    //         maxArea=Math.max(currArea,maxArea);
-    //      }
-    //   }
-      return maxArea;
+        int left=0;
+        int n=height.length;
+        int right=n-1;
+        int maxArea=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                int currArea=height[left]*(right-left);
+                maxArea=Math.max(currArea,maxArea);
+                left++;
+            }
+            else{
+                int currArea=height[right]*(right-left);
+                maxArea=Math.max(currArea,maxArea);
+                right--;
+            }
+        }
+        return maxArea;
     }
 }
