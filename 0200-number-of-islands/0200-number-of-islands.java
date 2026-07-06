d261245b483a4b2c4041c2408749dd2d3b54340f
Rajan Singh <72873770+rajansingh2206@users.noreply.github.com>
2026-07-06 11:01:50 +0530

    Time: 4 ms (46.39%), Space: 52.1 MB (72.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,51 +1,29 @@
 class Solution {
-    boolean vis[][];
-
-    class Pair {
-        int first;
-        int second;
-
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
-    private void bfs(int row,int col,char[][] grid,int n,int m){
-        vis[row][col]=true;
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(row,col));
-        int dr[]={-1,1,0,0};
-        int dc[]={0,0,-1,1};
-        while(!q.isEmpty()){
-        Pair pair=q.remove();
-        int newRow=pair.first;
-        int newCol=pair.second;
-            for(int k=0;k<4;k++){
-                int nRow=newRow+dr[k];
-                int nCol=newCol+dc[k];
-                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && vis[nRow][nCol]==false && grid[nRow][nCol]=='1'){
-                    q.add(new Pair(nRow,nCol));
-                    vis[nRow][nCol]=true;
-                }
-            }
-    }
-       
-}
-
     public int numIslands(char[][] grid) {
-        int count=0;
         int n=grid.length;
         int m=grid[0].length;
-        vis=new boolean[n][m];
+        boolean vis[][]=new boolean[n][m];
+        int total=0;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && vis[i][j]==false){
-                    count++;
-                    bfs(i,j,grid,n,m);
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(grid,vis,i,j,m,n);
+                    total++;
                 }
             }
         }
-        return count;
+        return total;
+    }
+    private void dfs(char[][] grid,boolean vis[][],int i,int j,int m,int n){
+
+        if(i<0 || j<0 || i>=n || j>=m || vis[i][j] || grid[i][j]!='1')
+            return;
+
+        vis[i][j]=true;
+        dfs(grid,vis,i-1,j,m,n);
+        dfs(grid,vis,i+1,j,m,n);
+        dfs(grid,vis,i,j-1,m,n);
+        dfs(grid,vis,i,j+1,m,n);
     }
 }
